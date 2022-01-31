4354b0e54a2ee6e0473c18a134c24d8464d34c0f
ahmedhhammad <98718441+ahmedhhammad@users.noreply.github.com>
2022-02-01 00:59:27 +0200

    Update download_playlist_pytube_en.py

--- download_playlist_pytube_en.py
+++ download_playlist_pytube_en.py
@@ -3,29 +3,40 @@
 from pytube import Playlist
 from pytube import YouTube
 
-p = Playlist(input("Enter the playlist link : "))
+Download_Dir = str(input("Enter the directory to save your downloades example: ( E:\Files\download ) : "))
 
-Download_Dir = str(input("Enter the directory to save your downloades like this ( E:\files\download ) : "))
-print(f'PlayList : {p.title}')
-
-
-
-start = int(input("\n" + "number of first video : "))-1
-end = int(input("number of last video : "))
-quality = str(input("download quality like ( 360 , 480 , 720 ) : "))
-
-for video in p.videos[start:end]:
-
-    video.streams.get_by_resolution(f"{quality}p").download(output_path=Download_Dir)
+def download_playlist():    
+    p = Playlist(input("Enter the playlist link : "))
+    print(f'PlayList : {p.title}')
     
+    start = int(input("\n" + "number of first video : "))-1
+    end = int(input("number of last video : "))
+    quality = str(input("download quality example ( 360 , 480 , 720 ) : "))
 
-    yt = YouTube(p.video_urls[start])
+    for video in p.videos[start:end]:
+
+        video.streams.get_by_resolution(f"{quality}p").download(output_path=Download_Dir)
+        
+        yt = YouTube(p.video_urls[start])
+        
+        size_Mg = int(yt.streams.get_by_resolution(f"{quality}p").filesize)/(1024*1024)
+        
+        print(f"Successfully Downloaded | {video.title} " + " | File_Size = " + str("{:.2f}".format(size_Mg)) + "MB")
+
+        start+=1
+              
+def decision():
+    global the_decision
     
-    size_Mg = int(yt.streams.get_by_resolution(f"{quality}p").filesize)/(1024*1024)
+    print("\n"+"Do you want to download more? ")
+    print("If Yes press y || If No press n")
+    the_decision = input("(yes or no) : ")
     
-    print(f"Successfully Downloaded | {video.title} " + " | File_Size = " + str("{:.2f}".format(size_Mg)) + "MB")
+    return the_decision
 
+download_playlist()     
+decision()
 
-    start+=1
-
-
+while str(the_decision) == "y":
+    download_playlist()     
+    decision()
