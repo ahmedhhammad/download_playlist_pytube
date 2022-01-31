42bc64f2089ff64d9bbe06983739912609942ce0
ahmedhhammad <98718441+ahmedhhammad@users.noreply.github.com>
2022-02-01 00:58:02 +0200

    Update download_playlist_pytube_ar.py

--- download_playlist_pytube_ar.py
+++ download_playlist_pytube_ar.py
@@ -3,37 +3,48 @@
 from pytube import Playlist
 from pytube import YouTube
 
-p = Playlist(input("رابط قائمة التشغيل : "))
+Download_Dir = str(input("اكتب مسار الملف الي هتحمل فيه الفيديوهات زي كدا مثلا ( E:\Files\download ) : "))
 
-Download_Dir = str(input("اكتب مسار الملف الي هتحمل فيه الفيديوهات زي كدا مثلا ( E:\files\download ) : "))
-print(f'قائمة التشغيل  : {p.title}')
-
-
-
-start = int(input("\n" + "رقم اول فيديو : "))-1
-end = int(input("رقم اخر فيديو : "))
-quality = str(input("جودة التحميل مثلا (360 او 480 او 720 ) : "))
-
-for video in p.videos[start:end]:
-
-    # دة عشان ابتدي احمل الفيديوهات وبأختار الجودة 
-    video.streams.get_by_resolution(f"{quality}p").download(output_path=Download_Dir)
+def download_playlist():    
+    p = Playlist(input("رابط قائمة التشغيل : "))
+    print(f'قائمة التشغيل  : {p.title}')
     
-    ## هنا جبت اللينك من البلاي ليست وعاملته كانه لينك فيديو عادي عشان اقدر احدد حجم الملف
-    # الخاص بالمكتبه كويس هتفهم انا لفيت اللفه دي ليه documentation لو قرأت ال  
-    #كان ممكن احصل علي البيانات الي عايزها باستخدام 
-    #  تانية بس كانت هتستهلك وقت ومعالجة اكتر for loop
-    
-    yt = YouTube(p.video_urls[start])
-    
-    ##هنا قدرت اجيب حجم الملف المحمل كاضافة بيانات للمستخدم مش اكتر بالجودة  المعينه محولة لميجا بايت
-    size_Mg = int(yt.streams.get_by_resolution(f"{quality}p").filesize)/(1024*1024)
-    
-    # دي مجرد بيانات هتظهر للمستخدم اثناء تحميل كل فيديو بحيث يبقي متابع البرنامج
-    print(f"Successfully Downloaded | {video.title} " + " | File_Size = " + str("{:.2f}".format(size_Mg)) + "MB")
+    start = int(input("\n" + "رقم اول فيديو : "))-1
+    end = int(input("رقم اخر فيديو : "))
+    quality = str(input("جودة التحميل مثلا (360 او 480 او 720 ) : "))
 
-    #دي عشان اتابع احجام الفيديوهات اعمل تريس للبرنامج هتعرف فايدتها
-    # ;)
-    start+=1
+    for video in p.videos[start:end]:
 
+        # دة عشان ابتدي احمل الفيديوهات وبأختار الجودة 
+        video.streams.get_by_resolution(f"{quality}p").download(output_path=Download_Dir)
+        
+        ## هنا جبت اللينك من البلاي ليست وعاملته كانه لينك عادي وبجيب منه البيانات يدوي
+        # الخاص بالمكتبه كويس هتفهم انا لفيت اللفه دي ليه documentation لو قرأت ال  
+        yt = YouTube(p.video_urls[start])
+        
+        ##هنا قدرت اجيب حجم الملف كاضافة للمستخدم مش اكتر بالجودة  المعينه وحولته لميجا بايت
+        size_Mg = int(yt.streams.get_by_resolution(f"{quality}p").filesize)/(1024*1024)
+        
+        # دي مجرد بيانات هتظهر للمستخدم اثناء تحميل كل فيديو بحيث يبقي متابع البرنامج
+        print(f"Successfully Downloaded | {video.title} " + " | File_Size = " + str("{:.2f}".format(size_Mg)) + "MB")
 
+        #دي عشان اتابع احجام الفيديوهات اعمل تريس للبرنامج هتعرف فايدتها
+        # ;)
+        start+=1
+              
+# دي عشان يقرر هيكمل تحميل تاني ولا يقفل البرنامج
+def decision():
+    global the_decision
+    
+    print("\n"+"Do you want to download more? ")
+    print("If Yes press y || If No press n")
+    the_decision = input("(yes or no) : ")
+    
+    return the_decision
+
+download_playlist()     
+decision()
+
+while str(the_decision) == "y":
+    download_playlist()     
+    decision()
